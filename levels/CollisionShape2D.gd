acb85e645c6aa47415a9e359edace49e0ae50efb
MBolivar <71595503+DragonicDemon01@users.noreply.github.com>
2021-06-11 16:38:55 -0300

    Update Level1.tscn

---
+++ levels/CollisionShape2D.gd
@@ -0,0 +1,18 @@
+extends CollisionShape2D
+
+
+# Declare member variables here. Examples:
+var Ax = 0
+var Ay = 0
+var Bx = 0
+var By = 0
+
+
+func _ready():
+	Ax = 
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
