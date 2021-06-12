507ef10e531db9afa1e8165c54ea8ef65e39a3dc
Adam Fowler <ajohnfowler@gmail.com>
2021-06-12 00:52:46 -0300

    Basic UI Designs

---
+++ ui/Main Menu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+func _on_Play_pressed():
+	SceneManager.goto_scene("res://Game.tscn")
---
+++ ui/Pause.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _on_Resume_pressed():
+	toogle_pause()
+
+func _on_Menu_pressed():
+	SceneManager.goto_scene('res://ui/Main Menu.tscn')
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+func _input(event):
+	if event.is_action_released("pause"):
+		toogle_pause()
+
+func toogle_pause():
+	get_tree().paused = not get_tree().paused
+	visible = get_tree().paused
