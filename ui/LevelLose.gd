4ade7bd920448278228f55cb41e7116cb9cef448
Adam Fowler <ajohnfowler@gmail.com>
2021-06-13 12:41:15 -0300

    Added audio players

--- ui/LevelLose.gd
+++ ui/LevelLose.gd
@@ -2,6 +2,7 @@ extends Panel
 
 func _on_Restart_pressed():
 	SceneManager.reload_scene()
+	$AudioStreamPlayer.play()
 
 func _on_Menu_pressed():
 	SceneManager.goto_scene('res://ui/Main Menu.tscn')
